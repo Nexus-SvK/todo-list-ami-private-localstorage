b2d423b325aaf4c0bb8f0d5564805a7079c84110
Matej <isoner.svk@protonmail.com>
2022-05-02 11:59:57 +0200

    Finished with localStorage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
-import TodoList from "./components/TodoList";
+import TodoList from "./components/TodoList/TodoList";
 import TodoForm from "./components/TodoForm/TodoForm";
 import React, { useState } from "react";
+import { useStateWithLocalStorage } from "./hooks/useStateWithLocalStorage";
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useStateWithLocalStorage("MyTodoList", []);
   const delTodo = (toids) => {
-    setTodos((todos) => todos.splice(toids, 1));
+    const updated = todos.slice();
+    updated.splice(toids, 1);
+    setTodos(updated);
   };
   const addTodo = (todoitem) => {
     setTodos([...todos, todoitem]);
